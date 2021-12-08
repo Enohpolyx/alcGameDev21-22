9fae80c293431b867c7c8bd9b8e8229b6475914b
Howard Crawford <howard.c285@stu.nebo.edu>
2021-12-08 14:20:08 -0700

    Began UI Controller

--- Prototype 3 - First Person/Assets/Scripts/Grog.cs
+++ Prototype 3 - First Person/Assets/Scripts/Grog.cs
@@ -14,7 +14,6 @@ public class Grog : MonoBehaviour
     public bool lowHealth = false;
     public bool lowAmmo = false;
 
-
     void Awake()
     {
         Speech.text = "";
@@ -24,23 +23,47 @@ public class Grog : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("ChangeBubble", 0, 0.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(player.curHP <= 20)
-        {
             lowHealth = true;
-            
-            
-            // if(!Bubble.activeSelf)
-            //     Bubble.SetActive(true);
-            // Speech.text = "You\'re low on health! Find some before you die!";
-        }
+        else if(player.curHP > 20)
+            lowHealth = false;
 
         if(weapon.curAmmo <= 20)
             lowAmmo = true;
+        else if(weapon.curAmmo > 20)
+            lowAmmo = false;
+    }
+
+            
+
+    void ChangeBubble()
+    {
+        if(lowHealth)
+        {
+            if(!Bubble.activeSelf)
+                Bubble.SetActive(true);
+            Speech.text = "You\'re low on health! Find some before you die!";
+        }
+
+        else if(lowAmmo)
+        {
+            if(!Bubble.activeSelf)
+                Bubble.SetActive(true);
+            Speech.text = "You\'re low on ammo! Better find some quick!";
+        }
+
+        else
+        {
+            if(Bubble.activeSelf)
+                Bubble.SetActive(false);
+            Speech.text = null;
+        }
+
     }
 }
